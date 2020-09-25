b8f1651dd045de4338f37df50ac6cf982fdeb19b
wangweiqi <wangweiqi@ichangtou.com>
2020-09-25 15:11:15 +0800

    搭建基本开发环境

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: "Example",
+      filename: "index.html",
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: "./dist",
+  },
+};
